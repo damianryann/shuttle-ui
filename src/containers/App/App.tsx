d48e8bc1bb217d9de0921057f5d7609dc5554e97
Damian Ryan <damian@damianryan.co.uk>
2022-05-16 11:05:27 +0100

    adding demo elements

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import logo from '../../assets/logo.svg';
 
 import { AsteroidBanner } from '../../components';
@@ -7,10 +7,19 @@ import './App.scss';
 interface AppProps {}
 
 const App: FunctionComponent<AppProps> = () => {
+  const [message, setMessage] = useState(false);
   const legal =
     'We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking "Accept All", you consent to our use of cookies.';
 
   const site = 'American Hobbit Pub';
+
+  function deleteCookie() {
+    const cookieName = `${site.replace(/\s/g, '')}Cookie` ?? 'AsteroidCookie';
+    document.cookie = `${cookieName}=; Max-Age=0; path=/;`;
+
+    setMessage(true);
+  }
+
   return (
     <div className="App">
       <AsteroidBanner
@@ -22,17 +31,15 @@ const App: FunctionComponent<AppProps> = () => {
       />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Check out the cookie banner!</p>
+        <button className="btn btn-lg btn-outline-light" onClick={deleteCookie}>
+          Delete cookie
+        </button>
+        {message && (
+          <div className="alert alert-success fs-5 mt-3" role="alert">
+            Please refresh the page!
+          </div>
+        )}
       </header>
     </div>
   );
